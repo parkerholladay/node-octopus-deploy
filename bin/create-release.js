3a1ad100ecbdf1c204a4636bf59c8462b16dd442
Agon Bina <agon.bina@storecast.de>
2017-12-05 14:05:41 +0100

    Pass through channelId for a simple release

--- bin/create-release.js
+++ bin/create-release.js
@@ -18,21 +18,39 @@ const args = yargs
   .describe('version', 'The SemVer of the release to create')
   .describe('releaseNotes', 'Notes to associate with the new release')
   .describe('packageVersion', 'The version of the packages to release')
+  .describe('channelId', 'The channel to be sued for the release deployments')
   .demandOption(['host', 'apiKey', 'projectSlugOrId', 'version'])
-  .example(`$0 \\\n --host=https://octopus.acme.com \\\n --apiKey=API-123 \\\n --projectSlugOrId={my-project|projects-123} \\\n --version=2.0.0-rc-4 \\\n --packageVersion=1.0.1 \\\n --releaseNotes="Created release as post-build step"`)
-  .argv
+  .example(
+    `$0 \\\n --host=https://octopus.acme.com \\\n --apiKey=API-123 \\\n --projectSlugOrId={my-project|projects-123} \\\n --version=2.0.0-rc-4 \\\n --packageVersion=1.0.1 \\\n --releaseNotes="Created release as post-build step"`
+  ).argv
 
-const { host, apiKey, projectSlugOrId, version, releaseNotes, packageVersion } = args
+const {
+  host,
+  apiKey,
+  projectSlugOrId,
+  version,
+  releaseNotes,
+  packageVersion,
+  channelId
+} = args
 
 octopusApi.init({ host, apiKey })
 
 logger.info(`Creating release for project '${projectSlugOrId}'...`)
 
-const params = { projectSlugOrId, version, releaseNotes, packageVersion }
+const params = {
+  projectSlugOrId,
+  version,
+  releaseNotes,
+  packageVersion,
+  channelId
+}
 
 createRelease(params)
   .then(release => {
-    logger.info(`Finished creating release '${release.id}'. ${projectSlugOrId} ${version}`)
+    logger.info(
+      `Finished creating release '${release.id}'. ${projectSlugOrId} ${version}`
+    )
     return release
   })
   .catch(err => {
