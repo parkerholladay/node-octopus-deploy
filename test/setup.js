3ffc5a8c07185bd75e219de937c16ab1c0c8fc41
Parker Holladay <parker.holladay@gmail.com>
2018-11-27 00:38:33 -0700

    Favor more fluent assertions with chai-as-promised
    * Ensure all tests with promises return if not using chai-as-promised

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,9 @@
 const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 const sinonChai = require('sinon-chai')
 
 chai.use(sinonChai)
+chai.use(chaiAsPromised)
 
 global.expect = chai.expect
 global.logToConsoleDisabled = true
